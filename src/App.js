8b47d618dea804a23725c11c85db0fec8352b6e6
github-cloudlabsuser-1327 <168090314+github-cloudlabsuser-1327@users.noreply.github.com>
2024-12-03 11:30:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import WelcomePopup from "./components/WelcomePopup";
 import AboutUs from "./components/AboutUs";
